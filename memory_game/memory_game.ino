73207fd7cf792d6b89fe2c0a843b474437ef8ad9
Anthony Rahajarison <anthony.rahajarison@laplateforme.io>
2025-04-10 16:23:18 +0200

    add memory game

---
+++ memory_game/memory_game.ino
@@ -0,0 +1,27 @@
+
+
+int blueLed = 7;
+int redLed = 6;
+int greenLed = 5;
+int yellowLed = 4;
+
+int blueButton = 13;
+int redButton = 12;
+int greenButton = 11;
+int yellowButton = 10;
+
+void setup() {
+    pinMode(blueLed, OUTPUT);
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    pinMode(yellowLed, OUTPUT);
+
+    pinMode(blueButton, INPUT);
+    pinMode(redButton, INPUT);
+    pinMode(greenButton, INPUT);
+    pinMode(yellowButton, INPUT);
+}
+
+void loop() {
+
+}
